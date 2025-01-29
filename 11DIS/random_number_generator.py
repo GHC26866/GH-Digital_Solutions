84a2d115a9af3f901c6c0ce5669c39155f756723
Geronimo Harman-Canales <26866@stpauls.qld.edu.au>
2025-01-29 12:35:18 +1000

    .

--- 11DIS/random_number_generator.py
+++ 11DIS/random_number_generator.py
@@ -1,1 +1,6 @@
 # Guess the number game
+# create a random number between 1 and 100
+# ask the user to guess the number
+# if the guess is too high, tell the user
+# if the guess is too low, tell the user
+
